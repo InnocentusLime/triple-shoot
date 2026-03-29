4445cf26396551735b1e9066c17be8e2ca820878
InnocentusLime <innocentuslime.help@gmail.com>
2026-03-29 17:35:40 +0300

    lib-game: remove grazing components
    The grazing mechanic is not going to be present in trishoot.

--- lib-game/src/components.rs
+++ lib-game/src/components.rs
@@ -17,15 +17,6 @@ pub enum Team {
     Enemy,
 }
 
-#[derive(Clone, Copy, Debug)]
-pub struct GrazeGain {
-    pub value: f32,
-    pub max_value: f32,
-}
-
-#[derive(Clone, Copy, Debug)]
-pub struct GrazeValue(pub f32);
-
 /// [Health] component stores entity's health.
 /// Normally, to do damage, you should just put it into the `damage` field.
 /// `damage` is zeroed every frame and is substracted to `value`.
@@ -76,39 +67,4 @@ impl Transform {
     pub fn from_xy(x: f32, y: f32) -> Self {
         Self::from_pos(vec2(x, y))
     }
-}
-
-#[derive(Debug, Clone, Copy)]
-pub struct CharacterLook(pub f32);
-
-impl CharacterLook {
-    pub fn to_direction(self) -> Vec2 {
-        Vec2::from_angle(self.0)
-    }
-
-    pub fn from_direction(dir: Vec2) -> Self {
-        Self(dir.to_angle())
-    }
-
-    pub fn to_dir_enum(self) -> Direction {
-        let angle = self.0;
-        let increment = std::f32::consts::FRAC_PI_4;
-        if (-3.0 * increment..=-increment).contains(&angle) {
-            Direction::Up
-        } else if (-increment..increment).contains(&angle) {
-            Direction::Right
-        } else if (increment..=3.0 * increment).contains(&angle) {
-            Direction::Down
-        } else {
-            Direction::Left
-        }
-    }
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum Direction {
-    Right = 0,
-    Down = 1,
-    Left = 2,
-    Up = 3,
 }
