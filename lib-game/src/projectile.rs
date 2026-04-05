257fd4fa68b0e5b90f48a4d9cd0f057dbf032ab5
InnocentusLime <innocentuslime.help@gmail.com>
2026-04-05 11:29:20 +0300

    lib-game: add piercing mechanic
    Bullets can now pierce enemies and opt out of that behaviour if they
    need to.

--- lib-game/src/projectile.rs
+++ lib-game/src/projectile.rs
@@ -9,10 +9,13 @@ pub fn step(dt: f32, world: &mut World) {
 }
 
 pub fn impact(world: &mut World, collisions: &CollisionSolver, cmds: &mut CommandBuffer) {
-    let mut query = world
-        .query::<(&KinematicControl, &Team, &col_query::Damage)>()
-        .with::<&ProjectileTag>();
-    for (projectile_entity, (kin, projectile_team, col_q)) in &mut query {
+    let mut query = world.query::<(
+        &KinematicControl,
+        &Team,
+        &col_query::Damage,
+        &mut ProjectileTag,
+    )>();
+    for (projectile_entity, (kin, projectile_team, col_q, tag)) in &mut query {
         if kin.collided {
             cmds.despawn(projectile_entity);
             continue;
@@ -20,17 +23,22 @@ pub fn impact(world: &mut World, collisions: &CollisionSolver, cmds: &mut Comman
 
         let mut collided = false;
         for collide_with in collisions.collisions_for(col_q) {
-            let Ok(collided_team) = world.get::<&Team>(*collide_with) else {
+            let Ok(mut query) = world.query_one::<(&Team, &Hp)>(*collide_with) else {
                 continue;
             };
-            if *collided_team == *projectile_team {
+            let Some((collided_team, collided_hp)) = query.get() else {
+                continue;
+            };
+            if *collided_team == *projectile_team || collided_hp.cooling_down() {
                 continue;
             }
             collided = true;
         }
 
-        if collided {
+        if collided && tag.pierce_count == 0 {
             cmds.despawn(projectile_entity);
+        } else {
+            tag.pierce_count = tag.pierce_count.saturating_sub(1)
         }
     }
 }
