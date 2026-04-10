2d4c3e3e9f922ed53e65634588775c515d3b8d4d
InnocentusLime <innocentuslime.help@gmail.com>
2026-04-10 15:08:09 +0300

    lib-game: add damage types

--- lib-game/src/prefab.rs
+++ lib-game/src/prefab.rs
@@ -36,6 +36,8 @@ pub fn register_libgame_components(prefab_factory: &mut PrefabFactory<Resources>
     prefab_factory.register_component::<ProjectileTag>("projectile");
     prefab_factory.register_component::<Team>("team");
     prefab_factory.register_component::<KnockbackTag>("knockback_effect");
+    prefab_factory.register_component::<Damage>("damage");
+    prefab_factory.register_component::<Defence>("defence");
 
     prefab_factory.register_component_with_constructor_ctx::<Sprite>("sprite");
 }
